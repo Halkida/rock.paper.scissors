eeccd95fa9e608d622b5c19052894cc1f04dfde2
Frolov Andrew <inham130@gmail.com>
2021-11-06 15:20:12 +0300

    Initial webpack config + Node version (#2)
    * gitignore fixed
    
    * initial webpack config. React. Typescript. SCSS. ESlint
    
    * CSS Modules
    
    * split global and modular styles
    
    * editor config has been added
    
    * webpack config adjustments
    
    * split development and production webpack config
    
    * node version

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import styles from'./app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className={styles.h1}>Hello React</h1>
+      </div>
+    );
+  }
+}
+export default App;
