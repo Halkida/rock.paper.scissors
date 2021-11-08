1e2260049c4c90377e01ce0a5a2d8ab21fbfc036
Andrey Frolov <inham130@gmail.com>
2021-11-08 18:46:23 +0300

    base input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,36 @@
+import { FC, InputHTMLAttributes } from 'react';
+import classNames from 'classnames/bind';
+import styles from'./Input.module.scss';
+
+type OwnProps = {
+  isValid?: boolean;
+  errorText?: string;
+  type?: 'number' | 'text' | 'password'
+} & InputHTMLAttributes<HTMLInputElement>;
+
+type Props = FC<OwnProps>;
+
+const cx = classNames.bind(styles);
+
+export const Input: Props = (
+  { isValid = true,
+    errorText = '',
+    type = 'text',
+    ...InputHTMLAttributes }
+  ) => {
+
+  const className = cx({
+    ['input']: true,
+    ['input_invalid']: !isValid
+  });
+
+  return (
+    <>
+      <input type={type} className={className} { ...InputHTMLAttributes } ></input>
+      {
+        (isValid === false && errorText.length > 0) &&
+        <span className={styles['error-text']} >{ errorText }</span>
+      }
+    </>
+  );
+};
