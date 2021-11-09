242c525fa7253fb6f100b732752d8b7e45059aae
Frolov Andrew <inham130@gmail.com>
2021-11-09 20:24:28 +0300

    Rps 18/base input component (#5)
    * webpack.common.js: add alias for import
    
    * tsconfig.json: add alias for import
    
    * base button component
    
    * .gitignore: .history added
    
    * Added classNames. Colors names adjusted. Link styles adjusted
    
    * base input component
    
    * input colors adjusted
    
    * color name fixed
    
    * index.scss: background-color fixed
    
    * input closing tag fixed
    
    * onChange recieve string insead of event
    
    * resolve package.lock.json conflict
    
    * resolve package-lock.json conflickts 2
    
    * resolve package-lock.json conflicts 3
    
    * return back react-router-dom
    
    * resolve package-lock.json conflicts 4
    
    Co-authored-by: Ryabchikova Anastasia <aryabchikova@ubic.tech>

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, FC, ButtonHTMLAttributes } from 'react';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from'./Button.module.scss';
+
+type OwnProps = {
+  view?: 'outline' | 'default' | 'link' | 'text';
+  size?: 'small' | 'medium' | 'large';
+  href?: string;
+  children: ReactNode | string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+type Props = FC<OwnProps>;
+
+const cx = classNames.bind(styles);
+
+export const Button: Props = (
+  { children,
+    view = 'default',
+    size = 'medium',
+    href = '',
+    ...ButtonHTMLAttributes }
+  ) => {
+
+  const className = cx({
+    ['button']: true,
+    [`button_${view}`]: view,
+    [`button_${size}`]: size,
+    ['button_link']: href.length
+  });
+
+  if (href.length > 0) {
+    return (
+      <Link to={href} className={className} >{children}</Link>
+    );
+  } else {
+    return (
+      <button
+        className={className}
+        { ...ButtonHTMLAttributes }
+      >
+        {children}
+      </button>
+    );
+  }
+};
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,49 @@
+import { FC, InputHTMLAttributes, ChangeEvent, useCallback } from 'react';
+import classNames from 'classnames/bind';
+import styles from'./Input.module.scss';
+
+type OwnProps = {
+  isValid?: boolean;
+  errorText?: string;
+  type?: 'number' | 'text' | 'password',
+  onChange?: (value: string) => void
+};
+
+type ReactInputAttributes = InputHTMLAttributes<HTMLInputElement>;
+type InputProps = OwnProps & Omit<ReactInputAttributes, keyof OwnProps>
+type Props = FC<InputProps>;
+
+const cx = classNames.bind(styles);
+
+export const Input: Props = (
+  { isValid = true,
+    errorText = '',
+    type = 'text',
+    onChange,
+    value,
+    ...InputHTMLAttributes }
+  ) => {
+
+  const className = cx({
+    ['input']: true,
+    ['input_invalid']: !isValid
+  });
+
+  const changeHandler = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (onChange) {
+      onChange(value);
+    }
+  }, [value]);
+
+  return (
+    <>
+      <input onChange={ changeHandler }
+      type={type} className={className} { ...InputHTMLAttributes } />
+      {
+        !isValid && errorText &&
+        <span className={styles['error-text']} >{ errorText }</span>
+      }
+    </>
+  );
+};
