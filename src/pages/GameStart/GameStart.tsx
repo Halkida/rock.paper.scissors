3306f7b9214d6b584d79ffebbd782e9df3e0ceab
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-11 22:11:24 +0300

    @/pages/GameStart: fix appearance

--- src/pages/GameStart/GameStart.tsx
+++ src/pages/GameStart/GameStart.tsx
@@ -1,10 +1,26 @@
 import { FC } from 'react';
-// import styles from'./GameStart.module.scss';
+import { Button } from '@/components/Button';
+import styles from'./GameStart.module.scss';
 
 const GameStart: FC = function GameStartPage() {
   return (
-    <div>
-      Начать игру
+    <div className={styles.page}>
+      <h1 className={styles.title}>
+        Начать игру
+      </h1>
+      <div className={styles.buttons}>
+        <Button
+          className={styles.button}
+          >
+          С комьютером
+        </Button>
+        <Button
+          className={styles.button}
+          view="outline"
+        >
+          С другим игроком
+        </Button>
+      </div>
     </div>
   );
 };
