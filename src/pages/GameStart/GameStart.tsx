34f22151b980a294505a41902b76d57684d3926d
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-11 08:56:42 +0300

    @/pages/GameStart/GameStart: create

---
+++ src/pages/GameStart/GameStart.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+// import styles from'./GameStart.module.scss';
+
+const GameStart: FC = function GameStartPage() {
+  return (
+    <div>
+      Начать игру
+    </div>
+  );
+};
+
+export default GameStart;
