46249e743f60901d6b4b9af5e895e5f35c0ec27b
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-14 19:41:21 +0300

    @/components/App/App: add game instance

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,24 @@
-import { Component } from 'react';
+import { FC, useState, useEffect } from 'react';
+import RPS from '@/RPS';
+import Gamer from '@/RPS/Gamer';
 import SignIn from '@/pages/SignIn';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/sign-in" element={ <SignIn /> } />
-        </Routes>
-      </Router>
-    );
-  }
-}
+const App: FC = function App() {
+  const [game, setGame] = useState<RPS | null>(null);
+
+  useEffect(() => {
+    const gamers = [new Gamer({ id: 2 }), new Gamer({ id: 1 })];
+    setGame(new RPS({ gamers }));
+  }, []);
+  console.log(game);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/sign-in" element={ <SignIn /> } />
+      </Routes>
+    </Router>
+  );
+};
+
 export default App;
