20ec78267f042be151428be2fcfc305f9f716bea
Andrey Frolov <inham130@gmail.com>
2021-11-14 18:52:33 +0300

    temporary rout to see result

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import SignIn from '@/pages/SignIn';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from '@/pages/SignUp';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <Routes>
           <Route path="/sign-in" element={ <SignIn /> } />
+          <Route path="/sign-up" element={ <SignUp /> } />
         </Routes>
       </Router>
     );
