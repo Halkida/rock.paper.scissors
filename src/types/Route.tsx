42c574f445de591b2ba6a9668e4cfde877aaa71a
kazakov.anton <as.kazakov@list.ru>
2021-11-14 23:27:42 +0500

    delete folder types

--- src/types/Route.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode, ReactElement } from 'react';
-
-export default interface Route {
-  path: string,
-  element: ReactElement | null;
-  isPrivate: boolean,
-  exact: boolean,
-  children?: ReactNode,
-}
