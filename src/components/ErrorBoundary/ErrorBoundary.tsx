ef3addf206bd9350f2c6ae2cee76f7447a3f6a81
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-15 22:06:41 +0300

    @/components/ErrorBoundary: add

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type Props = Record<string, never>;
+
+type State = {
+  hasError?: boolean;
+}
+
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Что-то пошло не так.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
