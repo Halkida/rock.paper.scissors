cdc551cbb48068cc75104e954bc79e43f6675f03
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-15 17:32:31 +0300

    @/pages/UsersRating/UserRating: add mocks

--- src/pages/UsersRating/UserRating.tsx
+++ src/pages/UsersRating/UserRating.tsx
@@ -2,12 +2,38 @@ import { FC } from 'react';
 import UserItem from './components/UserItem';
 import styles from './UserRating.module.scss';
 
-const data: [] = [];
+type User = {
+  id: number;
+  index: number;
+  name: string;
+  starsCount: number;
+};
+
+const data: User[] = [
+  {
+    id: 1,
+    index: 1,
+    name: 'Ваня Иванов',
+    starsCount: 100,
+  },
+  {
+    id: 2,
+    index: 2,
+    name: 'Ваня Пупкин',
+    starsCount: 70,
+  },
+  {
+    id: 3,
+    index: 3,
+    name: 'Ваня Пупкин',
+    starsCount: 50,
+  },
+];
 
 const UserRating: FC = function UserRating() {
   return (
-    <div>
-      <h1>
+    <div className={styles.page}>
+      <h1 className={styles.title}>
         Рейтинг игроков
       </h1>
       <div className={styles.container}>
