fe38c51151b0c714acf5224766bb329a8f8d0c72
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-16 09:06:18 +0300

    @/components/App/App: add ErrorBoundary

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,27 @@
 import { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from '@/pages/SignIn';
 import GameStart from '@/pages/GameStart';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ErrorBoundary from '../ErrorBoundary';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Routes>
-          <Route path="/sign-in" element={ <SignIn /> } />
-          <Route path="/game" element={ <GameStart /> } />
-        </Routes>
-      </Router>
+      <ErrorBoundary>
+        <div>
+          {
+            <h1>
+              {new Error()}
+            </h1>
+          }
+          <Router>
+            <Routes>
+              <Route path="/sign-in" element={ <SignIn /> } />
+              <Route path="/game" element={ <GameStart /> } />
+            </Routes>
+          </Router>
+        </div>
+      </ErrorBoundary>
     );
   }
 }
