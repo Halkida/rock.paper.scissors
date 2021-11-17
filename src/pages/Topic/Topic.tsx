d4eccf33b88fae0c35ad38d5a6f91c8174305974
Andrey Frolov <inham130@gmail.com>
2021-11-18 00:13:59 +0300

    topic page

--- src/pages/Topic/Topic.tsx
+++ src/pages/Topic/Topic.tsx
@@ -1,9 +1,53 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
+import mockTopicData from './mockTopicData';
+import styles from'./Topic.module.scss';
+
+interface Comment {
+  id: number;
+  author: string;
+  content: string;
+}
+
+interface Topic {
+  id: number
+  description: string;
+  author: string; // shoud be IUser
+  title: string;
+  commentsCount: number;
+  comments: Comment[];
+}
+
+type Nullable<T> = T | null;
 
 export const Topic: FC = () => {
+  const [topic, setTopic] = useState<Nullable<Topic>>(null);
+
+  useEffect(() => {
+    setTopic(mockTopicData);
+  }, []);
+
   return (
-    <main>
-      <h1>Topic</h1>
+    <main className={styles.page}>
+      <div className={styles.topic}>
+        { topic &&
+          <>
+            <h1 className={styles.topic_title}>{topic.title}</h1>
+            <div className={styles.topic_author}>{topic.author}</div>
+            <div className={styles.topic_description}>{topic.description}</div>
+
+            <ul className={styles.comments_list}>
+              { topic.comments.map((comment: Comment) => {
+                return (
+                  <li key={comment.id} className={styles.item}>
+                    <div className={styles.comment_author}>{comment.author}</div>
+                    <div className={styles.comment_content}>{comment.content}</div>
+                  </li>
+                );
+              }) }
+            </ul>
+          </>
+        }
+      </div>
     </main>
   );
 };
---
+++ src/pages/Topic/mockTopicData.ts
@@ -0,0 +1,18 @@
+const mockTopicData = {
+  id: 1,
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  author: 'Автор',
+  title: 'Название топика',
+  commentsCount: 3000,
+  comments: [{
+    id: 1,
+    author: 'Автор коммента',
+    content: 'Контент первого коммента'
+  }, {
+    id: 2,
+    author: 'Автор коммента',
+    content: 'Контент второго коммента'
+  }]
+};
+
+export default mockTopicData;
