11d960cc13e293e1f937af89eea3079f811840a2
Andrey Frolov <inham130@gmail.com>
2021-11-17 23:11:27 +0300

    Forum page

---
+++ src/pages/Forum/mockTopicListData.ts
@@ -0,0 +1,78 @@
+const moсkTopicList = [{
+  id: 1,
+  author: 'Хороший автор',
+  title: 'Не менее хороший тайтл',
+  commentsCount: 3000
+}, {
+  id: 2,
+  author: 'Автор похуже',
+  title: 'Обычный тайтл, стоящий внимания',
+  commentsCount: 2000
+}, {
+  id: 3,
+  author: 'Молодой подающий надежды автор',
+  title: 'Как же он хорош!',
+  commentsCount: 1000
+}, {
+  id: 4,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 5,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 6,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 7,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 8,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 9,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 10,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 11,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 12,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 13,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 14,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}, {
+  id: 15,
+  author: 'Еще один автор',
+  title: 'Не стоит внимяния',
+  commentsCount: 10
+}];
+
+export default moсkTopicList;
