36a2e35e420fe516d6f47a91802b70b0434f9886
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-20 15:10:48 +0300

    @/pages/Game: add Game

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,9 +1,55 @@
-import { FC } from 'react';
+import { FC, useEffect, useState, SyntheticEvent } from 'react';
+import RPS from '@/RPS';
+import Gamer from '@/RPS/Gamer';
+import { cardsTitles, Cards } from '@/RPS/constants';
+import styles from './Game.module.scss';
 
 export const Game: FC = () => {
+  
+  const [game, setGame] = useState<RPS>();
+  useEffect(() => {
+    setGame(new RPS({
+      gamers: [
+        new Gamer({ id: 1 }),
+        new Gamer({ id: 2 }),
+      ],
+    }));
+  }, []);
+
+  const handleCardClick = (id: number) => (e: SyntheticEvent<HTMLButtonElement>) => {
+    const { card }: { card?: Cards } = e.currentTarget.dataset;
+    if (!card) {
+      return;
+    }
+    game?.makeAStep(id, card)
+  };
+
   return (
     <main>
-      <h1>Game</h1>
+      <div className={styles.gamers}>
+        {game?.gamers.map(({
+          id,
+        }) => (
+          <div
+            key={id}
+          >
+            <div>
+              {`Gamer #${id}`}
+            </div>
+            {Object.keys(cardsTitles)
+              .map((card: Cards) => (
+                <button
+                  key={card}
+                  type="button"
+                  data-card={card}
+                  onClick={handleCardClick(id)}
+                >
+                  {cardsTitles[card]}
+                </button>
+              ))}
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
