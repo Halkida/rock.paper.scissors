dd6d1c38b843f8bb792fcb0c57e4ae1e4364730e
Frolov Andrew <inham130@gmail.com>
2021-11-24 15:49:25 +0300

    Rps 7/forum (#15)
    * App.tsx: fixed unnecessary div
    
    * Forum page
    
    * ForumIndex: fixed unnecessary <main> tag
    
    * topic page
    
    * user interface has been added
    
    * topic author has been added
    
    * Image component has been added
    
    * reset default styles of <ul> tag
    
    * mock author data in comments
    
    * Topic: Comment component has been added
    
    * Topic: decomposition
    
    * mockTopicData: added avatar src
    
    * IUser: import fixed
    
    * comment author adjusted
    
    * topic data: random avatar
    
    * mockTopicListData.ts: author string replaced by user object
    
    * Topic: author data adjusted
    
    * TopicListData: name changed
    
    * Topic: unused classes removed
    
    * Forum: IUser integrated. Columns adjusted

--- src/pages/ForumIndex/ForumIndex.tsx
+++ src/pages/ForumIndex/ForumIndex.tsx
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom';
 
 export const ForumIndex: FC = () => {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <Outlet />
   );
 };
--- src/pages/Topic/Topic.tsx
+++ src/pages/Topic/Topic.tsx
@@ -1,9 +1,60 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
+import { Image } from '@/components/Image';
+import { Comment } from './components/Comment';
+import { IUser } from '@/types';
+import mockTopicData from './mockTopicData';
+import styles from'./Topic.module.scss';
+
+interface Comment {
+  id: number;
+  author: IUser;
+  content: string;
+}
+
+interface Topic {
+  id: number
+  description: string;
+  author: IUser;
+  title: string;
+  commentsCount: number;
+  comments: Comment[];
+}
+
+type Nullable<T> = T | null;
 
 export const Topic: FC = () => {
+  const [topic, setTopic] = useState<Nullable<Topic>>(null);
+
+  useEffect(() => {
+    setTopic(mockTopicData);
+  }, []);
+
   return (
-    <main>
-      <h1>Topic</h1>
+    <main className={styles.page}>
+      <div className={styles.topic}>
+        { topic &&
+          <>
+            <h1 className={styles.topic_title}>{topic.title}</h1>
+            <div className={styles.topic_author}>
+              <span>{topic.author.nickName}</span>
+              <Image src={topic.author.avatar} width='80px' height='80px' alt='Аватар' />
+            </div>
+            <div className={styles.topic_description}>{topic.description}</div>
+            <div className={styles.comments}>
+              <ul>
+                { topic.comments.map((comment: Comment) => {
+                  return (
+                    <li key={comment.id}>
+                      <Comment author={comment.author} content={comment.content} />
+                    </li>
+                  );
+                }) }
+              </ul>
+            </div>
+
+          </>
+        }
+      </div>
     </main>
   );
 };
