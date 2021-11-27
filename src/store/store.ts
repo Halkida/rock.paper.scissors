054d27e7f4b56a6a628812fbc9301c053a742243
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-27 13:02:16 +0300

    @/store/store.ts: add thunk

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { IState } from '@/types';
 import reducer from './reducers';
 
 export function configureStore(initialState: IState) {
-  const store = createStore(reducer, initialState);
+  const store = createStore(reducer, initialState, applyMiddleware(thunk));
   return store;
 }
