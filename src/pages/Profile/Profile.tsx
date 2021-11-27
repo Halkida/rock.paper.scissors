5c231a9c0889edfe5e7600985a9674b64eff14cc
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-11-27 13:03:51 +0300

    @/pages/Profile/Profile: add profile output

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,23 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { IState, IUser } from '@/types';
 
 export const Profile: FC = () => {
+  const user: Nullable<IUser> = useSelector((state: IState) => state.user.user);
+  const keys = user && Object.keys(user) as (keyof IUser)[];
   return (
     <main>
       <h1>Profile</h1>
+      {keys && keys.length > 0 && (
+        <ul>
+          {keys.map((key) => (
+            <li key={key}>
+              <span>{`${key}: `}</span>
+              <span>{user[key]}</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 };
