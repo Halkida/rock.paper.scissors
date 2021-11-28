e56042ab0742f4d342837e908b4d9cd04c7b8931
Andrey Frolov <inham130@gmail.com>
2021-11-28 15:45:04 +0300

    Button tests: link test

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,10 +1,24 @@
 import { Button } from '../';
 import * as renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
 
-test('Button', () => {
-  const component = renderer.create(
-    <Button>Button</Button>
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+
+describe('Button component testing', () => {
+  test('Button', () => {
+    const component = renderer.create(
+      <Button>Button</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('Link', () => {
+    const component = renderer.create(
+      <MemoryRouter>
+        <Button href='https://ya.ru/'>Link</Button>
+      </MemoryRouter>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
