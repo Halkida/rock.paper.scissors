8090f2382a0f05187519bd997cc4d67b55c166ef
Frolov Andrew <inham130@gmail.com>
2021-12-01 10:54:28 +0300

    Rps 24/full screen api (#18)
    * Document and HTMLElement interfaces extended
    
    * useFullScreen hook has been added
    
    * global styles: background transparent for fullscreen element
    
    * useFullScreen refactored

---
+++ src/hooks/useFullSccreen.tsx
@@ -0,0 +1,44 @@
+import { useCallback, useState } from 'react';
+
+export const useFullScreen = () => {
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+
+  const requestFullScreen = useCallback((element: HTMLElement) => {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    } else if (element.mozRequestFullscreen) {
+      element.mozRequestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    }
+
+    setIsFullScreen(true);
+  }, []);
+
+  const exitFullScreen = useCallback(() => {
+    if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+
+    setIsFullScreen(false);
+  }, []);
+
+  const toggleFullScreen = useCallback((element: HTMLElement = document.body) => {
+    if (isFullScreen) {
+      exitFullScreen();
+    } else {
+      requestFullScreen(element);
+    }
+  }, [isFullScreen]);
+
+  return {
+    isFullScreen,
+    toggleFullScreen
+  };
+};
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,1 +1,16 @@
+interface Document {
+  mozCancelFullScreen?: () => Promise<void>;
+  msExitFullscreen?: () => Promise<void>;
+  webkitExitFullscreen?: () => Promise<void>;
+  mozFullScreenElement?: Element;
+  msFullscreenElement?: Element;
+  webkitFullscreenElement?: Element;
+}
+
+interface HTMLElement {
+  msRequestFullscreen?: () => Promise<void>;
+  mozRequestFullscreen?: () => Promise<void>;
+  webkitRequestFullscreen?: () => Promise<void>;
+}
+
 type Nullable<T> = T | null;
