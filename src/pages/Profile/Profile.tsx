cf514141fb74df5d8404b32455fff50d0305bf41
kazakov.anton <as.kazakov@list.ru>
2021-12-06 07:38:21 +0500

    add callback for edit form

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,12 +1,25 @@
-import { FC } from 'react';
-import UserFormData from '@/pages/Profile/UserFormData';
-import UserFormPassword from '@/pages/Profile/UserFormPassword';
+import { FC, useState } from 'react';
 import styles from'./Profile.module.scss';
 import { useSelector } from 'react-redux';
+import cx from 'classnames';
 import { IUser } from '@/types';
 import { selectUser } from '@/store/user/selectors';
+import { Button } from '@/components/Button';
+import UserFormData from '@/pages/Profile/Forms/UserFormData';
+import UserFormPassword from '@/pages/Profile/Forms/UserFormPassword';
 
 export const Profile: FC = () => {
+  const [isEditData, setIsEditData] = useState(false);
+  const [isEditPassword, setIsEditPassword] = useState(false);
+  const changeData = () => {
+    setIsEditPassword(false);
+    setIsEditData(!isEditData);
+  }
+  const changePassword = () => {
+    setIsEditData(false);
+    setIsEditPassword(!isEditPassword);
+  }
+
   const user: Nullable<IUser> = useSelector(selectUser) as IUser;
   const defaultAvatarText =  user?.first_name[0] + user?.second_name[0] || '';
   
@@ -20,8 +33,30 @@ export const Profile: FC = () => {
         <span className={styles.avatar__default}>{ defaultAvatarText }</span>
         </label>
       </div>
-      <UserFormData />
-      <UserFormPassword />
+      <div className={ cx([
+        styles.profilePage__formsWrapper,
+        { [styles.profilePage__formsWrapperIsEditPassword]: isEditPassword },
+        { [styles.profilePage__formsWrapperIsEditData]: isEditData },
+    ])}>
+        <UserFormData isEdit={isEditData} isEditPassword={isEditPassword} onEdit={changeData}/>
+        <UserFormPassword isEdit={isEditPassword} onEdit={changePassword}/>
+      </div>
+      <div className={ styles.profilePage__buttonsWrapper }>
+        <Button
+          type="button"
+          onClick={changeData}
+          className={styles.profilePage__buttonChange}
+        >
+          Изменить данные
+        </Button>
+        <Button
+          type="button"
+          onClick={changePassword}
+          className={styles.profilePage__buttonChange}
+        >
+          Изменить пароль
+        </Button>
+      </div>
     </main>
   );
 };
