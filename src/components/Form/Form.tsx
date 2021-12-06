09d726726ce1ed5984c9e4f45a5b00cf27ae375f
kazakov.anton <as.kazakov@list.ru>
2021-12-06 07:37:42 +0500

    add class for form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,10 @@
 import { FC, FormEvent } from 'react';
+import classNames from 'classnames/bind';
 import { Button } from '@/components/Button';
 import styles from'./Form.module.scss';
 
 type OwnFormProps = {
+  className?: string,
   onSubmit: (event: FormEvent<HTMLFormElement>) => void;
   renderFields: () => JSX.Element,
   submitText?: string
@@ -10,14 +12,21 @@ type OwnFormProps = {
 };
 type FormProps = FC<OwnFormProps>;
 
+const cx = classNames.bind(styles);
+
 export const Form: FormProps = function Form(
   { onSubmit,
     renderFields,
     title,
-    submitText = 'Отправить' }
-  ) {
+    submitText = 'Отправить',
+    className = '',
+  }) {
+  const formClassNames = cx({
+    [className]: true,
+    ['form']: true,
+  })
   return (
-    <div className={ styles.form }>
+    <div className={ formClassNames }>
       { title &&
         <span className={ styles.form__title }>
           {title}
