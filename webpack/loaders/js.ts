f4a5774b5f32e53f56f693475b419c5bfabff081
Andrey Frolov <inham130@gmail.com>
2021-12-12 21:59:52 +0300

    ssr webpack config

---
+++ webpack/loaders/js.ts
@@ -0,0 +1,22 @@
+import { ROOT_DIR } from '../env';
+
+const path = require('path');
+
+export default {
+  client: {
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: { loader: 'ts-loader' },
+      options: {
+        configFile: path.resolve(ROOT_DIR, 'tsconfig.json')
+      }
+  },
+  server: {
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: { loader: 'ts-loader' },
+      options: {
+        configFile: path.resolve(ROOT_DIR, '../tsconfig.json')
+      }
+  },
+};
