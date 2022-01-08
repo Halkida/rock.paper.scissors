fe0bdc3c49d0a1b18b9f7182fafec722a70919b2
Andrey Frolov <inham130@gmail.com>
2022-01-08 23:58:36 +0300

    @server: JSON parser has been installed

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import path from 'path';
 import express from 'express';
 import compression from 'compression';
+import bodyParser from "body-parser";
 import router from './server/router/router';
 import { dbConnect } from "@/server/initSequilize";
 
 const app = express();
+const jsonParser = bodyParser.json();
+
 
 app.use(compression())
   .use(express.static(path.resolve(__dirname, '../dist')))
   .use(express.static(path.resolve(__dirname, '../static')))
+  .use(jsonParser)
   .use(router);
 
 export function startApp() {
