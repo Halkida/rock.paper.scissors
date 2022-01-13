69198638fba3e915a263331dce3555fdbb4b947f
Andrey Frolov <inham130@gmail.com>
2022-01-13 21:32:55 +0300

    @server/router: user router has been introduced

--- src/server/router/router.ts
+++ src/server/router/router.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import serverApp from '@/server/serverRenderApp';
 import { themesRoutes } from './themesRoutes';
+import { userRoutes } from './userRoutes';
 
 const router = express.Router();
 
 themesRoutes(router);
+userRoutes(router);
 router.get('/*', serverApp);
 
 export default router;
