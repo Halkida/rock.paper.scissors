7e5df3c528d0574e097f25d2e1198d29bb4f8016
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2022-01-16 11:01:16 +0300

    @/services/theme: create

---
+++ src/services/theme.ts
@@ -0,0 +1,47 @@
+import { AxiosInstance } from 'axios';
+import { Themes } from '@/constants/themes';
+import getAxiosInstance from '@/utils/axios';
+
+export type GetTheme = {
+  ownerId: number,
+};
+
+export type ThemeParams = {
+  theme: Themes,
+};
+
+export type PostTheme = GetTheme & ThemeParams;
+
+class ThemeApi {
+  http: AxiosInstance;
+
+  constructor() {
+    this.http = getAxiosInstance();
+  }
+
+  getTheme(params: GetTheme): Promise<ThemeParams> {
+    console.log(params);
+    return Promise.resolve({
+      theme: Themes.dark,
+    });
+    // return this.http
+    //   .get<GetTheme>(
+    //       '/theme',
+    //       { params },
+    //     )
+    //   .then((response) => response.data);
+  }
+
+  postTheme(data: PostTheme): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(() => { resolve(); }, 1000);
+    });
+    // return this.http.post(
+    //   '/theme',
+    //   data,
+    // );
+  }
+
+}
+
+export default new ThemeApi();
