c740b8befc42f9ac7bf8f3e6991ff2f53fafa2d7
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2022-01-17 08:55:53 +0300

    @/pages/Profile/components/ThemeChange/ThemeChange: add title output

--- src/pages/Profile/components/ThemeChange/ThemeChange.tsx
+++ src/pages/Profile/components/ThemeChange/ThemeChange.tsx
@@ -2,24 +2,26 @@ import { FC, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUserTheme } from '@/store/user/selectors';
 import { postTheme } from '@/store/user/actions';
-import { Themes } from '@/constants/themes';
+import { THEMES, THEMES_TITLES } from '@/constants/themes';
 import SliderCheckbox from '@/components/SliderCheckbox';
 
 export const ThemeChange: FC = () => {
   const theme = useSelector(selectUserTheme);
   const dispatch = useDispatch();
   const handleSliderCheckboxChange = useCallback((value: boolean) => {
-    const theme = value ? Themes.dark : Themes.light;
+    const theme = value ? THEMES.dark : THEMES.light;
     dispatch(postTheme(theme));
   }, []);
 
   return (
     <div>
-      Цветовая тема: светлая
+      Цветовая тема:
+      {' '}
+      {THEMES_TITLES[theme]}
       {' '}
       <SliderCheckbox
         value="theme"
-        checked={theme === Themes.dark}
+        checked={theme === THEMES.dark}
         onChange={handleSliderCheckboxChange}
       />
     </div>
