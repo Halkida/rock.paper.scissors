b8f42abf0caccc4ee82c7578a8b7b48d6362e762
Andrey Frolov <inham130@gmail.com>
2022-01-18 23:09:40 +0300

    @pages/Forum: refactored to fit design and backend logic

--- src/pages/Forum/Forum.tsx
+++ src/pages/Forum/Forum.tsx
@@ -1,49 +1,62 @@
-import { FC, useState, useEffect  } from 'react';
-import { Link } from 'react-router-dom';
-import { IUser } from '@/types/User';
-import moсkTopicList from './mockTopicListData';
+import { FC, useState, useEffect } from 'react';
+import { TopicItem } from '@/components/TopicItem';
+import { Button } from '@/components/Button';
+import { CreateTopicModal } from './CreateTopicModal';
+import { useModal } from '@/components/Modal';
+import rpsImage from '@/assets/rps.png';
 import styles from'./Forum.module.scss';
 
 interface TopicListItem {
   id: number;
-  author: IUser;
+  authorInfo: {
+    avatar: string,
+    login: string
+  };
   title: string;
-  commentsCount: number
+  content: string;
+  commentsCount: number;
 }
 type TopicList = TopicListItem[];
 
 export const Forum: FC = () => {
   const [topicList, setTopicList] = useState<TopicList>([]);
+  const { isShown, toggle } = useModal();
+
+  const topic = {
+    id: 1,
+    authorInfo: {
+      avatar: '...',
+      login: 'SUPER_LOGIN'
+    },
+    title: 'Камень ножницы бумага лучшая игра',
+    content: 'Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный универсальный код речей. Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный универсальный код речей',
+    commentsCount: 300
+  };
 
   useEffect(() => {
-    setTopicList(moсkTopicList);
+    setTopicList([topic, topic]);
   }, []);
 
   return (
     <main className={styles.page}>
-      <h1>Форум</h1>
-      <div className={styles.forum}>
-        <table className={styles.forum_table}>
-          <thead>
-            <tr>
-              <th>Автор</th>
-              <th>Название</th>
-              <th>Комментов</th>
-            </tr>
-          </thead>
-          <tbody>
-            { topicList.map((topic: TopicListItem) => {
-              return (
-                <tr key={topic.id}>
-                  <td>{topic.author.display_name}</td>
-                  <td><Link className='link' to="/forum/1" >{topic.title}</Link></td>
-                  <td>{topic.commentsCount}</td>
-                </tr>
-              );
-            }) }
-          </tbody>
-        </table>
+      <div className={styles.head}>
+        <div className={styles.head_logo}>
+          <img
+            className={styles.logo}
+            src={rpsImage}
+          />
+        </div>
+        <div className={styles.head_button}>
+          <Button onClick={toggle} view='outline'>Создать тему</Button>
+        </div>
       </div>
+      <div className={styles.forum}>
+        {topicList.map((topic) => <TopicItem key={topic.id} {...topic} />)}
+      </div>
+      <CreateTopicModal
+        isShown={isShown}
+        toggle={toggle}
+      />
     </main>
   );
 };
