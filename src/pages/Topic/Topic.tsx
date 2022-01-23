6e04c5796ce08c75cd683975de61ef2e9f53b988
Andrey Frolov <inham130@gmail.com>
2022-01-23 14:07:10 +0300

    @/pages/Topic: fetch topic data from server

--- src/pages/Topic/Topic.tsx
+++ src/pages/Topic/Topic.tsx
@@ -1,7 +1,9 @@
 import { FC, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { TopicItem } from '@/components/TopicItem';
 import rpsImage from '@/assets/rps.png';
 import { Comment } from './components/Comment';
+import topicService from '@/services/topic';
 import styles from'./Topic.module.scss';
 
 const comment = {
@@ -33,26 +35,29 @@ interface Topic {
   title: string;
   content: string;
   commentsCount?: number;
-  comments: Comment[];
 }
 
-type Nullable<T> = T | null;
-
 export const Topic: FC = () => {
-  const [topic, setTopic] = useState<Nullable<Topic>>(null);
+  const [topic, setTopic] = useState<Topic>();
+  const { id: topicId = 0 } = useParams();
 
   useEffect(() => {
-    setTopic({
-      id: 1,
-      authorInfo: {
-        avatar: '...',
-        login: 'SUPER_LOGIN'
-      },
-      title: 'Камень ножницы бумага лучшая игра',
-      content: 'Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный универсальный код речей. Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный универсальный код речей',
-      commentsCount: 300,
-      comments: []
-    });
+    const fetchData = async () => {
+      const topic = await topicService.getTopic(Number(topicId));
+      const normilizedTopic = {
+        id: topic.id,
+        title: topic.title,
+        content: topic.content,
+        commentsCount: 0,
+        authorInfo: {
+          avatar: topic.avatar ? topic.avatar : '',
+          login: topic.login
+        }
+      };
+      setTopic(normilizedTopic);
+    };
+
+    fetchData();
   }, []);
 
   return (
