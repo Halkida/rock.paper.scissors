73f76ecdb0dc5b68165152e6f41cd8ed218000fd
Andrey Frolov <inham130@gmail.com>
2022-01-30 00:46:46 +0300

    prevent db tablse droppong

--- src/server/initSequilize.ts
+++ src/server/initSequilize.ts
@@ -20,7 +20,7 @@ sequelize.addModels([User, UserTheme, Topic, Comment]);
 export async function dbConnect() {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ force: true });
+    await sequelize.sync();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
