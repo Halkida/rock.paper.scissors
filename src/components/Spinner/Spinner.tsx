eeb93280a01f99427c22a93fe9cb0da2c6024bbc
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2022-01-30 13:04:58 +0300

    @/components/Spinner: add types

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,13 +2,30 @@ import { FC } from 'react';
 import cx from 'classnames';
 import styles from './Spinner.module.scss';
 
-export const Spinner: FC = () => {
+type OwnProps = {
+  type?: 'overlayed' | 'inline' | 'block',
+  className?: string,
+};
+
+export const Spinner: FC<OwnProps> = ({
+  type = 'overlayed',
+  className = '',
+}) => {
   return (
-    <div className={styles.spinner}>
-      <div className={cx([styles.blob, styles.top])} />
-      <div className={cx([styles.blob, styles.bottom])} />
-      <div className={cx([styles.blob, styles.left])} />
-      <div className={cx([styles.blob, styles.moveBlob])} />
+    <div
+      className={cx([styles.overlay, className])}
+    >
+      <div
+        className={cx([
+          styles.spinner,
+          styles[type],
+        ])}
+      >
+        <div className={cx([styles.blob, styles.top])} />
+        <div className={cx([styles.blob, styles.bottom])} />
+        <div className={cx([styles.blob, styles.left])} />
+        <div className={cx([styles.blob, styles.moveBlob])} />
+      </div>
     </div>
   );
 };
