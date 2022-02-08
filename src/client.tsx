060be963156f998d58f94f9159b3978d8df44524
Frolov Andrew <inham130@gmail.com>
2022-02-08 10:31:23 +0300

    rps-26/serrvice-worker (#45)
    * webpack: define plugin and global variable PRODUCTION has been added
    
    * SW cache logic
    
    * SW only in production build

--- src/client.tsx
+++ src/client.tsx
@@ -4,11 +4,17 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@/store';
 import ErrorBoundary from '@/components/ErrorBoundary';
 import App from './components/App';
+import { startServiceWorker } from './startServiceWorker';
 import './styles/index.scss';
 
 const initialState = window.__INITIAL_STATE__;
 const store = configureStore(initialState);
 
+// @ts-ignore
+if (PRODUCTION) {
+  startServiceWorker();
+}
+        
 hydrate(
   <Router>
     <ErrorBoundary>
