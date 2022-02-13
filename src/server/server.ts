161962e5d868599cb0990b5297e0dc41cd55c91c
Andrey Frolov <inham130@gmail.com>
2022-02-13 18:43:08 +0300

    server/server.ts: HTTPS enabled. requestUserMiddleware applied

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,15 +1,20 @@
-import path from 'path';
+import path, { join } from 'path';
 import express from 'express';
 import compression from 'compression';
 import bodyParser from 'body-parser';
+import https from 'https';
+import { readFileSync } from 'fs';
+import cookieParser from 'cookie-parser';
 import router from '@/server/router/router';
 import { dbConnect } from '@/server/initSequilize';
+import { requestUserMiddleware } from './middlewares/requestUser';
 
 const app = express();
 const jsonParser = bodyParser.json();
 
-
 app.use(compression())
+  .use(cookieParser())
+  .use(requestUserMiddleware)
   .use(express.static(path.resolve(__dirname, '../dist')))
   .use(express.static(path.resolve(__dirname, '../static')))
   .use(express.static('public'))
@@ -19,10 +24,20 @@ app.use(compression())
 export function startApp() {
   dbConnect()
     .then(() => {
-      const port = process.env.PORT || 3000;
+      const port = 443;
 
-      app.listen(port, () => {
-        console.log(`Application is started on localhost:${port}`);
-      });
+      if (process.env.NODE_ENV !== 'production') {
+        const options = {
+          cert: readFileSync(join(__dirname, 'certs', 'local.ya-praktikum.tech.pem'), 'utf-8'),
+          key: readFileSync(join(__dirname, 'certs', 'local.ya-praktikum.tech-key.pem'), 'utf-8'),
+        };
+        https.createServer(options, app).listen(port, () => {
+          console.log(`Application is started with https on port:${port}`);
+        });
+      } else {
+        app.listen(port, () => {
+          console.log(`Application is started on localhost:${port}`);
+        });
+      }
     });
 }
