5cdf3b1e5370c8f4656df4f40a96894760c2ec81
Andrey Frolov <inham130@gmail.com>
2022-02-13 18:42:27 +0300

    server/middlewares" request user middleware

---
+++ src/server/middlewares/requestUser.ts
@@ -0,0 +1,19 @@
+import {Request, Response, NextFunction} from 'express';
+import authApi from '@/services/auth.ts';
+
+export const requestUserMiddleware = async (req: Request, res: Response, next: NextFunction) {
+  const cookies = req.cookies;
+  console.log(cookies);
+  try {
+    const user = await authApi.getUser({
+      headers: { cookie: cookies },
+    });
+    res.user = user;
+    console.log(user);
+    next();
+  } catch (e: unknown) {
+    console.log('|||||||||||||||||||||||||||||||||||||||');
+    console.log(e.config.headers);
+    next();
+  }
+}
